49e3b51f8c76296fdc10e76c8c50414379b8e071
LuosRestil <briansmithwebdeveloper@gmail.com>
2024-05-21 11:11:40 -0400

    basic shapes

--- main.cpp
+++ main.cpp
@@ -4,6 +4,12 @@
 #include "Sphere.h"
 #include "Light.h"
 
+struct Intersection
+{
+    std::optional<Sphere*> sphere;
+    double t;
+};
+
 class RayTracer
 {
 public:
@@ -14,6 +20,7 @@ public:
     sf::Sprite sprite;
     std::vector<sf::Uint8> pixels;
     // scene
+    Vec3 bg_color{0, 0, 0};
     Vec3 camera_pos{0, 0, 0};
     double projection_plane_distance = 1.0;
     std::vector<Sphere> spheres = {
@@ -75,8 +82,52 @@ public:
 
     Vec3 trace_ray(const Vec3 &origin, const Vec3 &direction, double min_t, double max_t)
     {
-        // TODO do the thing
-        return {(direction.x + 0.5) * 255, 0, (1 - (direction.y + 0.5)) * 255};
+        Intersection intersection = get_nearest_intersection(origin, direction, min_t, max_t);
+        if (!intersection.sphere)
+            return bg_color;
+        
+        Sphere *sphere = intersection.sphere.value();
+        return sphere->color;
+    }
+
+    Intersection get_nearest_intersection(const Vec3 &origin, const Vec3 &direction, double min_t, double max_t)
+    {
+        std::optional<Sphere*> nearest_sphere = std::nullopt;
+        double nearest_t = INFINITY;
+
+        for (Sphere &sphere : spheres)
+        {
+            std::array<double, 2> ts = intersect_ray_sphere(origin, direction, sphere);
+            double t1 = ts[0];
+            double t2 = ts[1];
+            if (t1 < nearest_t && t1 > min_t && t1 < max_t)
+            {
+                nearest_t = t1;
+                nearest_sphere = &sphere;
+            }
+            if (t2 < nearest_t && t2 > min_t && t2 < max_t)
+            {
+                nearest_t = t2;
+                nearest_sphere = &sphere;
+            }
+        }
+        return {nearest_sphere, nearest_t};
+    }
+
+    std::array<double, 2> intersect_ray_sphere(const Vec3 &origin, const Vec3 &direction, const Sphere &sphere)
+    {
+        Vec3 CO = origin - sphere.center;
+        double a = direction.dot(direction);
+        double b = 2 * direction.dot(CO);
+        double c = CO.dot(CO) - sphere.radius * sphere.radius;
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0)
+            return {INFINITY, INFINITY};
+
+        double t1 = (-b + std::sqrt(discriminant)) / (2 * a);
+        double t2 = (-b - std::sqrt(discriminant)) / (2 * a);
+        return {t1, t2};
     }
 
     void put_pixel(int x, int y, const Vec3 &pixel_color)
